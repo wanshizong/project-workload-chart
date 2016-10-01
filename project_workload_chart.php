f495617465f1157b90cd2a6b30d6503ad1c6d0e2
wanshizong <marvin.chen@foxmail.com>
2016-10-01 16:19:27 +0800

    Create project_workload_chart.php

---
+++ project_workload_chart.php
@@ -0,0 +1,12 @@
+<?php
+  //Get the font path
+  putenv('GDFONTPATH='.realpath('.'));
+  
+  //open the database and get the project record
+  //Todo
+ 
+ $width = 1000;
+  $height = 1000;
+  $left_margin = 30;
+  $right_margin = 30;
+?>
